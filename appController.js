d75521f9070dea6f025c74c350df2b59fc1d31f5
Vineet Sinha <vineet.sinha@gmail.com>
2019-05-27 10:33:45 -0400

    moving code into a seperate controller and data access library

---
+++ appController.js
@@ -0,0 +1,161 @@
+////////////////////////////////////////
+// building out the conroller
+////////////////////////////////////////
+
+
+// the main controller
+class AppController {
+  constructor(plannedMeals) {
+    this.plannedMeals = plannedMeals
+  }
+
+  // utils
+  makeDevEasier(response) {
+    var dayProps = ['mealDay', 'newMealDay'];
+    dayProps.forEach(d=>{
+      if (response.get(d) === 'today') response.set(d, new Date());
+      if (response.get(d) === 'tomorrow') {
+        var mDay = new Date();
+        mDay.setDate(day.getDate()+1)
+        response.set(d, mDay);
+      }
+    });
+  }
+
+  // create records
+  planMeal(response) {
+    this.makeDevEasier(response);
+
+    if (response.get('mealDay').indexOf('W') != -1) {
+      // we do not support week's as days
+      response.say(`Sorry. Can you please provide a day?`)
+      return;
+    }
+
+    this.plannedMeals.write({
+      user_id: response.get('userId'),
+      meal_day: response.get('mealDay'),
+      meal_name: response.get('mealName'),
+      meal_type: response.get('mealType')
+    });
+    response.say(`Great.`);
+  }
+
+  // query records
+  async anyMealsPlannedTonight(response) {
+    var results = await this.plannedMeals.checkDay({
+      user_id: response.get('userId'),
+      meal_day: new Date().toISOString(),
+    });
+    if (results.length > 0) {
+      // check mealType
+      for (let mp of results) {
+        if (mp.meal_type==='dinner') {
+          response.set('mealName', mp.meal_name);
+          return 'planned';
+        }
+      }
+      return 'plannedOnlyOtherMeals';
+    }
+    results = await this.plannedMeals.checkWeek({
+      user_id: response.get('userId'),
+      meal_day: new Date().toISOString(),
+    });
+    if (results.length > 0) {
+      return 'plannedOnlyLaterInTheWeek';
+    }
+    return 'nonePlanned';
+  }
+
+  async anyMealsPlannedGeneric(response) {
+    this.makeDevEasier(response);
+    var results = await this.plannedMeals.checkDay({
+      user_id: response.get('userId'),
+      meal_day: response.get('mealDay'),
+    });
+    if (results.length > 0) {
+      // check mealType
+      var availMealType = null;
+      for (let mp of results) {
+        if (mp.meal_type===response.get('mealType')) {
+          response.set('mealName', mp.meal_name);
+          return 'planned';
+        }
+        availMealType = mp.meal_type;
+      }
+      // because we have one result - we will def have a value in availMealType
+      response.set('availMealType', availMealType);
+      return 'plannedButDifferentMeal';
+    }
+    results = await this.plannedMeals.checkWeek({
+      user_id: response.get('userId'),
+      meal_day: response.get('mealDay'),
+    });
+    if (results.length > 0) {
+      response.set('availMealDayPretty', this._dateToRelDay(results[0].meal_date_ex));
+      return 'plannedButOtherTimeInWeek';
+    }
+    return 'nonePlannedThisWeek';
+  }
+
+  _dateToRelDay(mealDateEx) {
+    var dayStr = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    var monthStr = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    var today = new Date();
+    mealDateEx = new Date(mealDateEx.substring(0, 'YYYY-MM-DD'.length));
+    var dateDiff = mealDateEx.getUTCDate()-today.getUTCDate();
+    switch (dateDiff) {
+      case 0: return 'today';
+      case -1: return 'yesterday';
+      case -2: return 'day before yesterday';
+      case 1: return 'tomorrow';
+      case 2: return 'day after tomorrow';
+    }
+    if (dateDiff<0 && dateDiff>-7) {
+      return `last ${dayStr[mealDateEx.getUTCDay()]}`;
+    }
+    if (dateDiff>0 && dateDiff<7) {
+      return `coming ${dayStr[mealDateEx.getUTCDay()]}`;
+    }
+    return ` ${mealDateEx.getUTCDate()} ${monthStr[mealDateEx.getUTCMonth()]}`;
+  }
+
+  async anyMealsPlannedOpenEnded(response) {
+    var results = await this.plannedMeals.checkWeek({
+      user_id: response.get('userId'),
+      meal_day: new Date().toISOString(),
+    });
+    if (!results) return 'err';
+    if (results.length == 0) return 'nonePlannedThisWeek'
+
+    // results are sorted by date and type (breakfast comes first)
+    var mp = results[0];
+    response.set('mealName', mp.meal_name);
+    response.set('mealType', mp.meal_type);
+    response.set('mealDay', mp.meal_date_ex);
+    response.set('mealDayPretty', this._dateToRelDay(mp.meal_date_ex));
+    return 'planned';
+  }
+
+  // update records
+  async updateDate(response) {
+    await this.plannedMeals.updateKey({
+      user_id: response.get('userId'),
+      meal_day: response.get('mealDay'),
+      meal_type: response.get('mealType'),
+      meal_name: response.get('mealName'),
+    }, {meal_day: response.get('newMealDay')});
+    response.set('mealDay', response.get('newMealDay'));
+  }
+  async updateMealType(response) {
+    await this.plannedMeals.update({
+      user_id: response.get('userId'),
+      meal_day: response.get('mealDay'),
+      meal_type: response.get('mealType'),
+      meal_name: response.get('mealName'),
+    }, {meal_type: response.get('newMealType')});
+    response.set('mealType', response.get('newMealType'));
+  }
+};
+
+module.exports = AppController;
--- plannedMeals.js
+++ plannedMeals.js
@@ -1,78 +1,42 @@
 ////////////////////////////////////////
-// building out the model / data access
+// building out the model
 ////////////////////////////////////////
-const AWS = require('aws-sdk');
-const uuidv1 = require('uuid/v1'); // timestamp based uuid
 
-AWS.config.update({
-  region: 'us-east-1',
-  endpoint: 'http://localhost:8000'
+const DynDB = require('./lib/dynDB');
+const dynDB = new DynDB({
+  collection: 'PlannedMealsCollection',
+  keys: ['user_id', 'meal_date_ex'],
+  query: {
+    keyConditionEx: {
+      'meal_date_ex': ['between', ':meal_day_beg', 'and', ':meal_day_end']
+    },
+    defaultLimit: 25
+  }
 });
 
-// var dynamodb = new AWS.DynamoDB();
+const uuidv1 = require('uuid/v1'); // timestamp based uuid
 
-var plannedMeals = {
-  write: async function({user_id, meal_day, meal_name, meal_type}) {
-    meal_day = meal_day.substring(0, 'YYYY-MM-DD'.length);
+const plannedMeals = {
+  write: async function(mealPlan /*{user_id, meal_day, meal_name, meal_type}*/) {
+    mealPlan.meal_day = mealPlan.meal_day.substring(0, 'YYYY-MM-DD'.length);
 
     var typeCode = 0;
-    switch (meal_type) {
+    switch (mealPlan.meal_type) {
       case 'breakfast': typeCode = 0; break;
       case 'lunch': typeCode = 1; break;
       case 'dinner': typeCode = 2; break;
     }
+    mealPlan.meal_date_ex = mealPlan.meal_day + '----' + typeCode + '----' + uuidv1();
+    delete mealPlan.meal_day; // we don't save it - can be retrieved from meal_date_ex
 
-    try {
-      var docClient = new AWS.DynamoDB.DocumentClient();
-      var saveParams = {
-        TableName: 'PlannedMealsCollection',
-        Item: {
-          user_id: user_id,
-          meal_date_ex: meal_day + '----' + typeCode + '----' + uuidv1(),
-          meal_name: meal_name,
-          meal_type: meal_type
-      }};
-      // console.log('Trying to write: ', JSON.stringify(saveParams, null, 2));
-      var data = await docClient.put(saveParams).promise();
-      // console.log('Added item:', JSON.stringify(data, null, 2));
-
-    } catch (err) {
-      console.error('Unable to add item. Error JSON:', JSON.stringify(err, null, 2));
-    }
+    await dynDB.insert(mealPlan);
   },
 
-  query: async function({user_id, meal_day_beg, meal_day_end, meal_name, meal_type}) {
-    meal_day_beg = meal_day_beg.substring(0, 'YYYY-MM-DD'.length);
-    meal_day_end = meal_day_end.substring(0, 'YYYY-MM-DD'.length);
+  query: async function(mealPlan /*{user_id, meal_day_beg, meal_day_end, meal_name, meal_type}*/) {
+    mealPlan.meal_day_beg = mealPlan.meal_day_beg.substring(0, 'YYYY-MM-DD'.length);
+    mealPlan.meal_day_end = mealPlan.meal_day_end.substring(0, 'YYYY-MM-DD'.length);
 
-    try {
-      var docClient = new AWS.DynamoDB.DocumentClient();
-      var queryParams = {
-        TableName: 'PlannedMealsCollection',
-        KeyConditionExpression: 'user_id = :user_id and meal_date_ex between :meal_day_beg and :meal_day_end',
-        ExpressionAttributeValues: {
-          ':user_id': user_id,
-          ':meal_day_beg': meal_day_beg,
-          ':meal_day_end': meal_day_end,
-        },
-        Limit: 25
-      };
-      console.log('Trying to query: ', JSON.stringify(queryParams, null, 2));
-      var data = await docClient.query(queryParams).promise();
-      console.log('Queried item:', JSON.stringify(data, null, 2));
-      // return data.Items;
-      return data.Items.filter(item=>{
-        // these two are already done in the query
-        // if (user_id && item.user_id !== user_id) return false;
-        // if (meal_day && item.meal_date_ex.substring(0, meal_day.length) !== meal_day) return false;
-        if (meal_name && item.meal_name !== meal_name) return false;
-        if (meal_type && item.meal_type !== meal_type) return false;
-        return true;
-      });
-
-    } catch (err) {
-      console.error('Unable to query items. Error JSON:', JSON.stringify(err, null, 2));
-    }
+    return await dynDB.query(mealPlan);
   },
 
   // this is what everyone will want to use instead of query - since we don't want to expose the actual meal_date_ex
@@ -102,7 +66,7 @@ var plannedMeals = {
 
   update: async function({user_id, meal_day, meal_name, meal_type}, newRecVals) {
     // because we dont actually have meal_date_ex and we need it to do an update - we will need to first do a query with a range
-    var results = await checkDay({user_id, meal_day, meal_type, meal_name});
+    var results = await this.checkDay({user_id, meal_day, meal_type, meal_name});
     var record = results[0];
     if (results.length>0) {
       // there was more than one entry with the same meal_name and meal_type
@@ -110,36 +74,13 @@ var plannedMeals = {
       console.log('WARNING: Changing to: ', newRecVals);
     }
 
-    try {
-      var docClient = new AWS.DynamoDB.DocumentClient();
-      var updateParams = {
-        TableName: 'PlannedMealsCollection',
-        Key: {
-          'user_id': record.user_id,
-          'meal_date_ex': record.meal_date_ex
-        },
-        UpdateExpression: 'set',
-        ExpressionAttributeValues: {},
-        ReturnValues:"UPDATED_NEW"
-      };
-      Object.keys(newRecVals).forEach(k=>{
-        if (updateParams.UpdateExpression.length!=3) updateParams.UpdateExpression+=',';
-        updateParams.UpdateExpression += ` ${k}=:${k}`;
-        updateParams.ExpressionAttributeValues[`:${k}`] = newRecVals[k];
-      });
-      console.log('Trying to do an update: ', JSON.stringify(updateParams, null, 2));
-      var data = await docClient.update(queryParams).promise();
-      console.log('Updated item:', JSON.stringify(data, null, 2));
-    } catch (err) {
-      console.error('Unable to query items. Error JSON:', JSON.stringify(err, null, 2));
-    }
-
+    await dynDB.update(record, newRecVals);
   },
 
   // dynamodb does not allow changing the value of keys!
   updateKey: async function({user_id, meal_day, meal_name, meal_type}, newRecVals) {
     // because we dont actually have meal_date_ex and we need it to do an update - we will need to first do a query with a range
-    var results = await checkDay({user_id, meal_day, meal_type, meal_name});
+    var results = await this.checkDay({user_id, meal_day, meal_type, meal_name});
     var record = results[0];
     if (results.length>0) {
       // there was more than one entry with the same meal_name and meal_type
@@ -147,30 +88,14 @@ var plannedMeals = {
       console.log('WARNING: Deleting and replacing with to: ', newRecVals);
     }
 
-    try {
-      var docClient = new AWS.DynamoDB.DocumentClient();
-      // delete
-      var deleteParams = {
-        TableName: 'PlannedMealsCollection',
-        Key: {
-          'user_id': record.user_id,
-          'meal_date_ex': record.meal_date_ex
-        }
-      };
-      console.log('Trying to delete: ', JSON.stringify(deleteParams, null, 2));
-      var data = await docClient.delete(queryParams).promise();
-      console.log('Deleted item:', JSON.stringify(data, null, 2));
+    await dynDB.delete(record);
 
-      // create
-      var newRec = {user_id, meal_day, meal_name, meal_type};
-      Object.keys(newRecVals).forEach(k=>{ // we are really doing an Object.assign here
-        newRec[k] = newRecVals[k];
-      });
-      await plannedMeals.write(newRec);
-    } catch (err) {
-      console.error('Unable to query items. Error JSON:', JSON.stringify(err, null, 2));
-    }
-
+    // create
+    var newRec = {user_id, meal_day, meal_name, meal_type};
+    Object.keys(newRecVals).forEach(k=>{ // we are really doing an Object.assign here
+      newRec[k] = newRecVals[k];
+    });
+    await plannedMeals.write(newRec);
 
   }
 
